a3553c51352cc985469cb346c0b21db95be9b9e6
temamtahir <temam123t@gmail.com>
2022-12-08 06:46:37 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,10 @@
  *
  * */
 
-int main(void){
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
