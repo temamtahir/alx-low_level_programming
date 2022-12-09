a775af2139889f1dc17222b7164be2fc6deba332
temamtahir <temam123t@gmail.com>
2022-12-09 13:07:18 -0800

    trailing

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,8 +9,8 @@
 int main(void)
 {
 	int n;
-	
-	srand(time(0));			
+
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if ((n % 10) > 5)
 	{
@@ -18,7 +18,7 @@ int main(void)
 	}
 	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",n, n % 10);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
 	else
 	{
