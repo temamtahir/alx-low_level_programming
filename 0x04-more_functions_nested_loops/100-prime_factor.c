91361f393faa876b4f23f720db26dce75a033d8d
temamtahir <temam123t@gmail.com>
2022-12-15 13:12:20 -0800

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 101-main.c 101-print_number.c -o 101-print_numbers

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 /**
- * * main - finds and prints the largest prime factor of the number 612852475143
- *
+ *main - finds and prints the largest prime factor of the number 612852475143
  * * followed by a new line
- *
  * * Return: Always 0 (Success)
- *
  */
 int main(void)
 {
-
 	long int n;
 	long int max;
 	long int i;
