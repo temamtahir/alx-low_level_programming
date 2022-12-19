962e1166ac3f1a51f9de8105240ff76710cd8209
temamtahir <temam123t@gmail.com>
2022-12-19 05:30:10 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,14 @@
 #include "main.h"
 
-/*
+/**
  * * puts2 - function should print only one character out of two
- *
  * * starting with the first one
- *
  * * @str: input
  *
  * * Return: print
  *
  */
-
 void puts2(char *str)
-
 {
 
 	int longi = 0;
