f984ff5305d1ec07c42ae09010922524f078ebdd
temamtahir <temam123t@gmail.com>
2022-12-22 03:20:06 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-main.c
+++ 0x06-pointers_arrays_strings/103-main.c
@@ -1,21 +1,10 @@
 #include "main.h"
-
 #include <stdio.h>
-
-
-
 /**
- *
  * * main - check the code
- *
- * *
- *
  * * Return: Always 0.
- *
- * */
-
+ */
 int main(void)
-
 {
 
 	char *n = "1234567892434574367823574575678477685785645685876876774586734734563456453743756756784458";
