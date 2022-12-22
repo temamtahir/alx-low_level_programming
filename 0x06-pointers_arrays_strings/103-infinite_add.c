46465ae9a4e92a8bcc427b7d15857f92ef63adf1
temamtahir <temam123t@gmail.com>
2022-12-22 03:14:15 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,116 +1,43 @@
 #include "main.h"
 
-#include <stdio.h>
-
 
 
 /**
- * * main - check the code
+ * * rev_string - reverse array
  *
- * *
+ * * @n: integer params
  *
- * * Return: Always 0.
+ * * Return: 0
  *
  */
-
-int main(void)
+void rev_string(char *n)
 
 {
 
-	char *n = "1234567892434574367823574575678477685785645685876876774586734734563456453743756756784458";
+	int i = 0;
 
-	char *m = "9034790663470697234682914569346259634958693246597324659762347956349265983465962349569346";
+	int j = 0;
 
-	char r[100];
-
-	char r2[10];
-
-	char r3[11];
-
-	char *res;
+	char temp;
 
 
 
-	res = infinite_add(n, m, r, 100);
-
-	if (res == 0)
+	while (*(n + i) != '\0')
 
 	{
 
-		printf("Error\n");
+		i++;
 
 	}
 
-	else
-
+	i--;
+	for (j = 0; j < i; j++, i--)
 	{
 
-		printf("%s + %s = %s\n", n, m, res);
+		temp = *(n + j);
 
+		*(n + j) = *(n + i);
+
+		*(n + i) = temp;
 	}
-
-	n = "1234567890";
-
-	m = "1";
-
-	res = infinite_add(n, m, r2, 10);
-
-	if (res == 0)
-
-	{
-
-		printf("Error\n");
-
-	}
-
-	else
-
-	{
-
-		printf("%s + %s = %s\n", n, m, res);
-
-	}
-
-	n = "999999999";
-
-	m = "1";
-
-	res = infinite_add(n, m, r2, 10);
-
-	if (res == 0)
-
-	{
-
-		printf("Error\n");
-
-	}
-
-	else
-
-	{
-
-		printf("%s + %s = %s\n", n, m, res);
-
-	}
-
-	res = infinite_add(n, m, r3, 11);
-
-	if (res == 0)
-
-	{
-
-		printf("Error\n");
-
-	}
-
-	else
-
-	{
-
-		printf("%s + %s = %s\n", n, m, res);
-
-	}
-
-	return (0);
-
 }
