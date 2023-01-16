eeca55ab224e82a27e36d38728e7a409ba2a6fda
temamtahir <temam123t@gmail.com>
2023-01-16 06:01:23 -0800

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c -o c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 
-
-
 /**
  * * main - prints the name of the file
- *
- * *
- *
  * * Return: Always 0 (Success)
  */
-int ain(void)
+
+int main(void)
 {
 
 	printf("%s\n", __FILE__);
-
 	return (0);
 
 }
