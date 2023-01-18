2b8bc8c904522a8dbbb6ce0bd8ff9960f6b1979c
temamtahir <temam123t@gmail.com>
2023-01-18 02:58:57 -0800

    updates

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * * main - prints its own opcodes 
+ * * main - prints its own opcodes
  * * @argc: number of arguments
  * * @argv: array of arguments
  * * Return: Always 0 (Success)
