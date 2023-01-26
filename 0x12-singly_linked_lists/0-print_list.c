0606563e637329a88066b24c9893b08928d3c27f
temamtahir <temam123t@gmail.com>
2023-01-26 02:21:37 -0800

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,17 +2,8 @@
 #include "lists.h"
 /**
  * * * print_list - prints all the elements of a linked list
- *
- * *
- *
  * * * @h: pointer to the list_t list to print
- *
- * *
- *
- * * *
- *
- * * * Return: the number of nodes printed
- *
+ * * Return: the number of nodes printed
  */
 size_t print_list(const list_t *h)
 {
