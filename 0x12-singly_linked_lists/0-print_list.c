13acdbbb82bfdc912eee36b405d6118af1322b2b
temamtahir <temam123t@gmail.com>
2023-01-26 02:18:33 -0800

    new ipdate

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * * print_list - prints all the elements of a linked list
- *
- * * @h: pointer to the list_t list to print
+ * * * print_list - prints all the elements of a linked list
  *
  * *
- * * Return: the number of nodes printed
-  */
+ *
+ * * * @h: pointer to the list_t list to print
+ *
+ * *
+ *
+ * * *
+ *
+ * * * Return: the number of nodes printed
+ *
+ */
 size_t print_list(const list_t *h)
 {
 	size_t s = 0;
