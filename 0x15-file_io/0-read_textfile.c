62de0b4c7b210cd7092280c68632e4a5340c884f
temamtahir <temam123t@gmail.com>
2023-02-06 18:31:42 -0800

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 
 /**
- *
  * * read_textfile- Read text file print to STDOUT.
  *
  * * @filename: text file being read
@@ -13,8 +12,7 @@
  * * Return: w- actual number of bytes read and printed
  *
  * *        0 when function fails or filename is NULL.
- *
- * */
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 
