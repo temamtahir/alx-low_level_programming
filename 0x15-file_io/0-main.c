31f0a5e3e2757a0c517560c75b89db6210a85324
temamtahir <temam123t@gmail.com>
2023-02-06 18:14:14 -0800

    ipdare

--- 0x15-file_io/0-main.c
+++ 0x15-file_io/0-main.c
@@ -4,7 +4,6 @@
 
 /**
  * * main - check the code
- * *
  * * Return: Always 0.
  */
 int main(int ac, char **av)
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,9 @@
 #ifndef MAIN_H
+
 #define MAIN_H
+
+
+
 #include <sys/types.h>
 
 #include <sys/stat.h>
@@ -14,6 +18,10 @@ ssize_t read_textfile(const char *filename, size_t letters);
 
 int create_file(const char *filename, char *text_content);
 
-int append_text_to_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content);
+
+
 
 #endif
+
+
